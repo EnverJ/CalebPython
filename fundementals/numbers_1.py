10fd67baa4f0900e4ffe2b784a626175721b3cb6
EnverJ <Enlem1987@gmail.com>
2025-10-07 21:53:34 -0600

    string

---
+++ fundementals/numbers_1.py
@@ -0,0 +1,29 @@
+age = 30
+print("My age is ", age)
+
+#camel case, pascal case, snake case
+
+#camelCase
+#PascalCase
+#snake_case
+
+result = 20/3
+# result = 20//3 # result is integer
+print(result)
+
+# multiplication
+num2 = 5 ** 3
+print(num2) #125
+
+# round
+result2 = round(20/3)
+print(result2) # 7
+result3 = round(5.6)
+print(result3) # 6
+
+num4 = 300
+# num4 = num4/9
+num4 /=9
+print(num4) # 6
+
+
